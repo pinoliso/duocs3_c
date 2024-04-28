90a3f739591653468b33e9dc7c18e7b73e25af15
Enrique Pino <pinoliso@gmail.com>
2024-04-28 09:41:55 -0400

    test publication service

---
+++ src/main/java/duoc/s3_c/exception/PublicationNotFoundException.java
@@ -0,0 +1,7 @@
+package duoc.s3_c.exception;
+
+public class PublicationNotFoundException extends Exception {
+    public PublicationNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/test/java/duoc/s3_c/service/PublicationServiceTest.java
@@ -0,0 +1,55 @@
+package duoc.s3_c.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.ArgumentMatchers.any;
+import java.util.Optional;
+
+import duoc.s3_c.model.Publication;
+import duoc.s3_c.repository.PublicationRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class PublicationServiceTest {
+
+    @InjectMocks
+    private PublicationService publicationService;
+
+    @Mock
+    private PublicationRepository publicationRepositoryMock;
+
+    @Test
+    public void createMovieServiceTest() {
+        
+        Publication publication = new Publication();
+        publication.setTitle("Publicación Save");
+
+        when(publicationRepositoryMock.save(any())).thenReturn(publication);
+
+        Publication savedPublication = publicationService.createPublication(publication);
+
+        assertEquals("Publicación Save", savedPublication.getTitle());
+    }
+
+    @Test
+    void deletePublicationTest() {
+
+        Publication publication = new Publication();
+        publication.setTitle("Publicación 1");
+        when(publicationRepositoryMock.save(any())).thenReturn(publication);
+        Publication savedPublication = publicationService.createPublication(publication);
+
+        
+        publicationService.deletePublication(savedPublication.getId());
+        
+        verify(publicationRepositoryMock, times(1)).deleteById(savedPublication.getId());
+    }
+    
+}
