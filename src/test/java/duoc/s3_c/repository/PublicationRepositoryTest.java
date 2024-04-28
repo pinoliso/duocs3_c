5b8986b03ac37b332eb372acfdd9deb1775de105
Enrique Pino <pinoliso@gmail.com>
2024-04-28 10:19:45 -0400

    test publication repository

--- src/test/java/duoc/s3_c/repository/PublicationRepositoryTest.java
+++ src/test/java/duoc/s3_c/repository/PublicationRepositoryTest.java
@@ -1,54 +1,47 @@
-package duoc.s3_c.service;
+package duoc.s3_c.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import static org.mockito.ArgumentMatchers.any;
-import java.util.Optional;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import duoc.s3_c.model.Publication;
-import duoc.s3_c.repository.PublicationRepository;
 
-@ExtendWith(MockitoExtension.class)
-public class PublicationServiceTest {
-
-    @InjectMocks
-    private PublicationService publicationService;
-
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class PublicationRepositoryTest {
+    
     @Mock
     private PublicationRepository publicationRepositoryMock;
 
     @Test
-    public void createMovieServiceTest() {
-        
+    public void savePublicationRepositoryTest() {
         Publication publication = new Publication();
         publication.setTitle("Publicación Save");
 
         when(publicationRepositoryMock.save(any())).thenReturn(publication);
 
-        Publication savedPublication = publicationService.createPublication(publication);
+        Publication savedPublication = publicationRepositoryMock.save(publication);
 
         assertEquals("Publicación Save", savedPublication.getTitle());
     }
 
     @Test
-    void deletePublicationTest() {
-
+    public void deletePublicationRespositoryTest() {
         Publication publication = new Publication();
         publication.setTitle("Publicación 1");
         when(publicationRepositoryMock.save(any())).thenReturn(publication);
-        Publication savedPublication = publicationService.createPublication(publication);
+        Publication savedPublication = publicationRepositoryMock.save(publication);
 
-        publicationService.deletePublication(savedPublication.getId());
+        publicationRepositoryMock.deleteById(savedPublication.getId());
         
         verify(publicationRepositoryMock, times(1)).deleteById(savedPublication.getId());
     }
-    
+
 }
